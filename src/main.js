ee55312d259d3fa0c558998f351029c1d5bc2cff
Matt Williams <mwilliams28291@googlemail.com>
2017-10-23 19:58:53 +0100

    Starting Module 2

---
+++ src/main.js
@@ -0,0 +1,32 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage.jsx');
+var About = require('./components/about/aboutPage.jsx');
+
+(function(win) {
+    "use strict";
+    var App = React.createClass({
+        render: function() {
+            var Child;
+
+            switch(this.props.route) {
+                case 'about': Child = About; break;
+                default: Child = Home;
+            }
+
+            return (
+                <div>
+                    <Child />
+                </div>
+            );
+        }
+    });
+
+    function render() {
+        var route = win.location.hash.substr(1);
+        React.render(<App route={route}/>, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', render);
+    render();
+})(window);
