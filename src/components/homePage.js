829a3349735064f240a4193118f7ac0fbb94c45d
Matt Williams <matt@mtwilliams.uk>
2017-10-25 13:12:27 +0100

    Starting the next module

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -5,7 +5,7 @@ var React = require('react');
 var Home = React.createClass({
     render: function() {
         return (
-            <div className="jumbotron container">
+            <div className="jumbotron">
                 <h1>Pluralsight Administration</h1>
                 <p>React, React Router, and Flux for ultra-responsive web apps.</p>
             </div>
